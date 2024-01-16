9eea9e7fc7237af939d75ca6246d0775c2b76c7d
priyanshu <patnayakpriyanshu06@gmail.com>
2024-01-16 14:46:15 +0530

    "chatRoom code"

--- src/main/java/com/priyanshu/webscoket/chatroom/ChatRoomRepository.java
+++ src/main/java/com/priyanshu/webscoket/chatroom/ChatRoomRepository.java
@@ -1,5 +1,9 @@
 package com.priyanshu.webscoket.chatroom;
 
-public interface ChatRoomRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
+public interface ChatRoomRepository  extends MongoRepository<ChatRoom , String> {
+    Optional<ChatRoom> findBySenderIdAndRecipientId(String senderId, String recipientId);
 }
